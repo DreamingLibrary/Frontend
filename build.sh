a94872c3307e3f9cb4fca916c98f5bfff788d0ec
송은수 <songess@naver.com>
2025-05-12 01:02:21 +0900

    [fix] : build.sh 수정 - cicd 설정

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-# 작업 디렉토리를 GitHub Actions 기본 작업 디렉토리로 유지
-mkdir -p ./output
-
-# frontend 디렉토리에서 파일 복사
+cd ../
+mkdir output
 cp -R ./frontend/* ./output
-
-# output 디렉토리를 다시 frontend로 복사
-cp -R ./output/* ./frontend/
+cp -R ./output ./frontend/
