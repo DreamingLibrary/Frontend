627ca05ec4224858eb5a88f61eac537d21744c4b
송은수 <songess@naver.com>
2025-05-12 00:50:59 +0900

    [fix] : build.sh 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-cd ../
-mkdir output
+# 작업 디렉토리를 GitHub Actions 기본 작업 디렉토리로 유지
+mkdir -p ./output
+
+# frontend 디렉토리에서 파일 복사
 cp -R ./frontend/* ./output
-cp -R ./output ./frontend/
+
+# output 디렉토리를 다시 frontend로 복사
+cp -R ./output/* ./frontend/
